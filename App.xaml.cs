5e4fb9a1754e354459a43592f2919e6161e0e3a0
Zhenghao Liu <uth9@outlook.com>
2025-08-17 23:12:39 +0800

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DocumentHelper
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
